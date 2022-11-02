32c1c03d9a3d7b5b6cd5ed36726a5bd134bfbd91
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2022-11-02 18:50:00 +0900

    scrollmagic library

--- js/src/page.js
+++ js/src/page.js
@@ -1,42 +1,27 @@
-(function ($) {
-  // onlyBannerBg-img translateX
-  let onlyBannerBgImg = $(".onlyBannerBg-img");
-  let bgImgH = onlyBannerBgImg.height();
-  let bgImgT = onlyBannerBgImg.offset().top;
-
-  $(window).on("scroll", function () {
-    imgTrans();
-    opacityFn();
-  });
-  function imgTrans() {
-    let scroll = $(window).scrollTop();
-    if (scroll < bgImgT || scroll > bgImgT + bgImgH) return;
-    onlyBannerBgImg.css({
-      transform: "translateY(" + (35 / bgImgH) * (scroll - bgImgT) + "%)",
-    });
-  }
-  // a-Opacity 에 is-OpacityTop 부여
-  let aOpacityTop = $(".a-OpacityTop");
-  let aOpacityTopT = aOpacityTop.offset().top;
-
-  function opacityFn() {
-    let sc = $(window).scrollTop();
-    let viewportHeight = $(window).height();
-
-    if (aOpacityTopT < viewportHeight + sc) {
-      aOpacityTop.addClass("is-OpacityTop");
-    }
-  }
-
-  let brandHeader = $(".brandHeader");
-  let brandHeaderT = brandHeader.offset().top;
-  function brandHeaderFn() {
-    let sc = $(window).scrollTop();
-    let viewportHeight = $(window).height();
-
-    if (brandHeaderT < viewportHeight + sc) {
-      let aClip = brandHeader.find(".a-Clip");
-      aClip.addClass("is-Clip");
-    }
-  }
-})(jQuery);
+const aOpacityTop = document.querySelectorAll(".a-OpacityTop");
+const aOpacity = document.querySelectorAll(".a-Opacity");
+const aClip = document.querySelectorAll(".a-Clip");
+aOpacityTop.forEach(function (opT) {
+  new ScrollMagic.Scene({
+    triggerElement: opT,
+    triggerHook: 0.8,
+  })
+    .setClassToggle(opT, "is-OpacityTop")
+    .addTo(new ScrollMagic.Controller());
+});
+aOpacity.forEach(function (opT) {
+  new ScrollMagic.Scene({
+    triggerElement: opT,
+    triggerHook: 0.8,
+  })
+    .setClassToggle(opT, "is-Opacity")
+    .addTo(new ScrollMagic.Controller());
+});
+aClip.forEach(function (clip) {
+  new ScrollMagic.Scene({
+    triggerElement: clip,
+    triggerHook: 0.8,
+  })
+    .setClassToggle(clip, "is-Clip")
+    .addTo(new ScrollMagic.Controller());
+});
